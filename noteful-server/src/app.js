fa5f656cb7d42c60f63264d49a0ede7179f6b91a
Rhudson <contact@rebeccahudson.me>
2020-08-03 07:58:51 -0400

    commit before heroku push

---
+++ noteful-server/src/app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const token = require('./token');
+const error = require('./error');
+
+const foldersRouter = require('./folders/folders-router');
+const notesRouter = require('./notes/notes-router');
+
+const app = express();
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+app.use(token);
+
+app.use(foldersRouter);
+app.use(notesRouter);
+
+app.use(error);
+
+module.exports = app;
